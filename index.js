ed66e3a6473f4925e74f56b75a410bf22d000788
ONDONDA Prince <pondonda@gmail.com>
2021-01-17 16:42:10 +0100

    changing port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const {UsersDataloader, MessagesDataloader, PublicationDataloader} = require("./
 // express server
 const app = express()
 
-const PORT = process.env.PORT_graphql;
+const PORT = process.env.PORT_graphql || 4000;
 
 const server = new ApolloServer({
   typeDefs,
